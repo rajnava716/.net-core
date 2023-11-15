23075deb4ecad037c2e4abbf0eccc6c294af1804
rajnava716 <150864072+rajnava716@users.noreply.github.com>
2023-11-14 20:59:45 -0600

    Sample file upload project

---
+++ ImportProgressBar/Controllers/DataImportController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Web.Http;
+using System.Threading.Tasks;
+
+
+namespace ImportProgressBar.Controllers
+{
+    [Route("[controller]")]
+    public class DataImportController : ApiController
+    {
+        [HttpPost]
+       
+        public async Task<IHttpActionResult> StartImport()
+        {
+            // Start the import process
+            var importTask = Task.Run(() => PerformDataImport());
+
+            // Return a response immediately
+            return Ok("Import process started");
+        }
+
+        private async Task PerformDataImport()
+        {
+            // Simulate data import progress
+            for (int progress = 0; progress <= 100; progress += 10)
+            {
+                // Simulate delay (replace with actual import logic)
+                await Task.Delay(1000);
+
+                // Update progress (you might use a message queue or other mechanism)
+                // For simplicity, storing it in a static variable
+                ProgressStorage.SetProgress(progress);
+            }
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetImportProgress()
+        {
+            var progress = ProgressStorage.GetProgress();
+            return Ok(new { progress });
+        }
+    }
+}
+
---
+++ ImportProgressBar/ProgressStorage.cs
@@ -0,0 +1,16 @@
+﻿namespace ImportProgressBar;
+
+public static class ProgressStorage
+{
+    private static int progress;
+
+    public static int GetProgress()
+        return progress;
+    }
+
+    public static void SetProgress(int newProgress)
+    {
+        progress = newProgress;
+    }
+}
+
